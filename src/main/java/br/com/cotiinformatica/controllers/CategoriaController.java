82e3cb069ef9fc53373a0ee338287346c0fb58b8
alexsandroe <alexsandroe@gmail.com>
2025-08-30 12:51:08 -0300

    criacao dos arquivos

---
+++ src/main/java/br/com/cotiinformatica/controllers/CategoriaController.java
@@ -0,0 +1,16 @@
+package br.com.cotiinformatica.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/categorias")
+public class CategoriaController {
+	@GetMapping
+	public ResponseEntity<?> getAll() {
+		// TODO Implementar o metodo da consulta APO
+		return ResponseEntity.ok().build();
+	}
+}
